98577cb085412ae911e6d3398d20c66e84ed2ab8
github username <github email address>
2018-06-04 17:49:08 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
+# Returns a hash of key/value pairs in which the keys are the original string
+# elements in the array, and their corresponding values are a count of the
+# number of times they appeared in the array.
 def count_elements(array)
-  # code goes here
+  helper_array = array
+  helper_hash = {}
+  helper_array.each do |string|
+    number_helper = array.count(string)
+    helper_hash[string] = number_helper
+  end
+  return helper_hash
 end
- 
