9411387ff72975d91c1afd618b4f9f3b88cc769c
Mary-Kate James <marykatj@gmail.com>
2018-06-07 00:42:42 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,13 +1,16 @@
+require 'pry'
+
 def count_elements(array)
   new_hash = {}
   count = 0
   array.collect do |animal|
-    if count = 0
+    if count == 0
       count += 1
       new_hash[animal] = count
     else
       count += 1
       new_hash[animal] = count
+    end
   end
   new_hash
 end
